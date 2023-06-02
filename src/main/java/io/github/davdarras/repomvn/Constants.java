ee78b05a30528148c1c6304ed2284a28b2cf91c6
David Darras <david.darras@insee.fr>
2023-06-02 15:07:56 +0200

    feat: update class

--- src/main/java/io/github/davdarras/repomvn/Constants.java
+++ src/main/java/io/github/davdarras/repomvn/Constants.java
@@ -1,8 +1,15 @@
-package io.github.davdarras;
+package io.github.davdarras.repomvn;
 
+/**
+ * Constants class
+ */
 public class Constants {
 	private String plop;
 
+	/**
+	 * Coonstructor for Constants
+	 * @param plop plop variable
+	 */
 	public Constants(String plop) {
 		this.plop = plop;
 	}
